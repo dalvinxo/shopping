61fc561f27323568af42c37426a6ee8f94614420
dalvinox <dalvingamer@gmail.com>
2020-11-11 03:15:21 -0400

    desig signup

--- app/src/main/java/com/example/shopping/screenMain/Login.java
+++ app/src/main/java/com/example/shopping/screenMain/Login.java
@@ -1,19 +1,47 @@
 package com.example.shopping.screenMain;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.Animation;
+import android.widget.Button;
 
+import com.example.shopping.MainActivity;
 import com.example.shopping.R;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
 public class Login extends AppCompatActivity {
 
-
+    Button back, signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        back = findViewById(R.id.btn_backlogin);
+        signup = findViewById(R.id.btn_signupLogin);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent start = new Intent(Login.this, ScreenSignup.class);
+                startActivity(start);
+                overridePendingTransition(R.anim.in_top,R.anim.out_top);
+            }
+        });
     }
 
 
@@ -23,7 +51,51 @@ public class Login extends AppCompatActivity {
         overridePendingTransition(R.anim.in_top, R.anim.out_top);
     }
 
+   /* private void alertSignup(){
+
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder (Login.this,android.R.style.Theme_Material_Dialog_NoActionBar);
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_signup,null);
+        builder.setView(view);
+        final AlertDialog dialog = builder.create();
+        dialog.show();
 
 
+        final CheckBox check = view.findViewById(R.id.politeCheckbox);
+        MaterialButton aceptar = view.findViewById(R.id.politeAceptada);
+        MaterialButton cancelar = view.findViewById(R.id.politeRechazada);
+
+        aceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (check.isChecked()){
+                    Toast.makeText(getApplicationContext(),"politicas aceptadas",Toast.LENGTH_SHORT).show();
+                    dialog.dismiss();
+                    dialogcorreo();
+
+                }else{
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),"Debes aceptar nuestras politica",Toast.LENGTH_SHORT).show();
+
+                        }
+                    },1000);
+                }
+
+            }
+        });
+
+
+
+        cancelar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+    }
+*/
 
 }
---
+++ app/src/main/java/com/example/shopping/screenMain/ScreenSignup.java
@@ -0,0 +1,18 @@
+package com.example.shopping.screenMain;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.shopping.R;
+import android.os.Bundle;
+
+public class ScreenSignup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_signup);
+
+
+
+
+    }
+}
